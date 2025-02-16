f0e8fe57c3e4907efc7daeb8e8dfed0479d83914
Cristian Morales <cristian.morales1@unmsm.edu.pe>
2025-02-16 17:31:12 -0500

    4.0.0

--- models/server.js
+++ models/server.js
@@ -27,15 +27,16 @@ class Server {
 }
 
 
-    middlewares(){
-        //CORS
-        this.app.use(cors());
-        //lectura y parseo del body recibe lo que se envia
-        this.app.use(express.json());
-        //directorio publico
-        this.app.use(express.static('public'));
+middlewares(){
+  //CORS
+  this.app.use(cors());
+  //lectura y parseo del body recibe lo que se envia
+  this.app.use(express.json());
+  //directorio publico
+  this.app.use(express.static('public'));
 
-    }
+}
+
 
   listen() {
     this.app.listen(this.port, () => {
--- routers/auth.js
+++ routers/auth.js
@@ -1,9 +1,23 @@
 const {Router} = require('express');
+const { validarCampos } = require('../middlewares/validar-campos');
+
 const { check } = require('express-validator');
-const { login } = require('../controller/auth');
+const { login, googleSignIn } = require('../controller/auth');
 const router = Router();
 
-router.post('/login', login );
+router.post('/login',[
+    check('correo','El correo es obligatorio').isEmail(),
+    check('password','El Password obligatorio ').not().isEmpty(),
+    validarCampos
+
+] ,login );
+
+router.post('/google',[
+    check('id_token','id_token es necesario ').not().isEmpty(),
+    validarCampos
+] ,googleSignIn );
+
+
 
 //const token = await generarJWT(usuario.id);
 module.exports = router;
